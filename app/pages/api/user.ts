0c0cb716a5bce49546c61e1d01bef3ef0cae15e4
Jonas Hahn <herzock@gmail.com>
2024-02-21 14:44:25 -0600

    update dependencies

--- app/pages/api/user.ts
+++ app/pages/api/user.ts
@@ -11,8 +11,6 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   }
   res.setHeader('Access-Control-Allow-Credentials', "true")
   res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
